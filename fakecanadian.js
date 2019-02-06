a34faf1c83c67756869cfa74e4bd20461df8ca28
Neil Okamoto <nhokamoto@dhcp-168-105-225-183.wireless.manoa.hawaii.edu>
2019-02-06 12:39:42 -1000

    Update fakecanadian.js

--- fakecanadian.js
+++ fakecanadian.js
@@ -1,6 +1,6 @@
-function fakeCanadian(array){
-  let addString = 'eh';
-  return _.map(array, function(string){return string + " " +addString});
+function fakeCanadian(array) {
+  const addString = ' eh';
+  return _.map(array, function (string) { return string + addString; });
 }
-let array = ['Would you like to eat?', 'No thank you.'];
+const array = ['Would you like to eat?', 'No thank you.'];
 console.log(fakeCanadian(array));
